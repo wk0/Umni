892175191a0646e0f1e0106c06a948cef2c6e632
Yves Aubourg <yaubourg59@gmail.com>
2018-02-17 19:38:00 -0700

    src files for UMNI Pricing Module

---
+++ consumer_app/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Yeezy Boost 350', desc: 'Footwear', price: '$202.71', qt: '431' },
+  { id: 2, name: 'Apple Earpods', desc: 'Headphones',  price: '$82.71', qt: '3231'  },
+  { id: 3, name: 'iPhone X', desc: 'Cellular Phone/Smart Phone', price: '$1021.22', qt: '319'  },
+  { id: 4, name: 'Macbook Pro', desc: 'Personal Computer',  price: '$1182.71', qt: '1201' },
+  { id: 5, name: 'Yeezy Boost 350', desc: 'Footwear', price: '$202.71', qt: '431' },
+];
